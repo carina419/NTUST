8191ccc994e87856077f7dcde686543dba026a79
chenn325 <119609242+carina419@users.noreply.github.com>
2023-09-25 16:25:38 +0800

    entropy 程式碼修改

--- MI5130701 資料探勘 Data Mining/112-1 Data Mining Coding/entropy code.py
+++ MI5130701 資料探勘 Data Mining/112-1 Data Mining Coding/entropy code.py
@@ -1,16 +1,17 @@
 import numpy as np
+
 def entropy(i, j):
     if i == 0 or j == 0:
         return 0
     p = i / (i + j)
     return - p*np.log2(p) - (1 - p)*np.log2((1 - p))
 
-a = input ('請輸入全部的總數：')
-b = input ('請輸入正確的總數：')
+# i = input ('請輸入全部的總數：') #numpy 好像只能用浮點數float
+# j = input ('請輸入正確的總數：')
 
 print('Informaiton Gain資訊獲得：')
 
-print(entropy(a,b))
+print(entropy(9,5))
 
 print ('test') 
 x=3 
